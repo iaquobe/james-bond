251d0589926266399e218c4460d6b4ec479826d2
Jakob Lambert-Hartmann <jakob.lamberthartmann.canada@gmail.com>
2025-06-18 18:16:11 +0200

    prototype

---
+++ analysis.py
@@ -0,0 +1,91 @@
+from ultralytics import YOLO
+import cv2
+from PIL import Image, ImageDraw
+import argparse
+import torch
+import clip
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+def box_to_coordinates(box): 
+    x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+    return x1, y1, x2, y2
+
+
+
+def crop_person(box, image) -> Image.Image : 
+    x1,y1, x2,y2 = box_to_coordinates(box)
+    img = image[y1:y2, x1:x2]
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    im_pil = Image.fromarray(img)
+    return im_pil
+
+
+
+def get_features(image_pil, features):
+    image = preprocess(image_pil).unsqueeze(0).to(device)
+    result = dict()
+
+    for feature, prompt in features.items():
+        text  = clip.tokenize(prompt).to(device)
+
+        with torch.no_grad():
+            logits_per_image, _ = clip_model(image, text)
+            probs               = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+        result[feature] = probs
+
+    return result
+
+
+
+def dominant_features(features, person_features): 
+    return {feature:features[feature][person_features[feature].argmax()] 
+        for feature in features.keys()}
+
+
+
+
+def detect_people(results): 
+    for result in results:
+        image   = result.orig_img
+        persons = result.boxes
+        path    = result.path
+
+        for person_box in persons:
+            cropped_person = crop_person(person_box, image)
+            person_features = get_features(cropped_person, features)
+
+            # cropped_person.show("cropped person")
+            plt.imshow(np.asarray(cropped_person))
+            print(person_features)
+            print(dominant_features(features, person_features))
+            plt.show()
+
+
+
+
+######################################################################
+## MAIN 
+######################################################################
+# if __name__ == "__main__": 
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("scene")
+
+features = {"wealth": ["a rich person", "a poor person"], 
+            "gender": ["a woman", "a man"]}
+
+# YOLO
+yolo    = YOLO("yolo11n.pt")
+results = yolo.predict("./scenes/shameless/", classes=[0], conf=0.6, stream=True)
+
+# CLIP
+device           = "cuda" if torch.cuda.is_available() else "cpu"
+clip_model, preprocess = clip.load("ViT-B/32", device=device)
+
+
+detect_people(results)
+
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from ultralytics import YOLO
 import cv2
 from PIL import Image, ImageDraw
+import torch
+import clip
 
 genders = {"male": 0, "female": 1, "break" : 2}
+features = {"wealth": ["a rich person", "a poor person"]}
 
 def box_to_coordinates(box): 
     x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
@@ -34,15 +37,82 @@ def prompt_gender(image, person):
 
 def annotate(): 
     model = YOLO("yolo11n.pt")
-    results = model.predict("./scenes/", classes=[0], conf=0.6, stream=True)
+    results = model.predict("./scenes/charade/", classes=[0], conf=0.6, stream=True)
 
     for result in results:
-        b=False
-        image = result.orig_img
+        image   = result.orig_img
         persons = result.boxes
+        path    = result.path
+
         for person in persons: 
             if prompt_gender(image, person) == genders["break"]: 
                 return
 
 
+
+annotate()
+
+
+
+model = YOLO("yolo11n.pt")
+results = model.predict("./scenes/charade/", classes=[0], conf=0.6, stream=True)
+
+result = next(results)
+while len(result.boxes) == 0: 
+    result = next(results)
+
+(x1,y1), (x2,y2) = box_to_coordinates(result.boxes[0])
+img = result.orig_img[y1:y2, x1:x2]
+
+cv2.imshow("cropped", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+im_pil = Image.fromarray(img)
+
+
+
+
+
+
+
+result.path.split(".")[0] + ".pred"
+result.path.split(".")[0] + ".gender"
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(im_pil).unsqueeze(0).to(device)
+text = clip.tokenize(["a woman", "a man"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  
+
+
+text = clip.tokenize(["a rich person", "a poor person"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  
+
+text = clip.tokenize(["a woman", "a man"]).to(device)
+
+with torch.no_grad():
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  
 
